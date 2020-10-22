2f816a8a0f89b3ae0a689f27a1471ccdf729b70d
Maya Chisem <chisem.maya@gmail.com>
2020-10-21 23:30:39 -0700

    figuring out front end styling for homepage

---
+++ public/assets/js/index.js
@@ -0,0 +1,12 @@
+new WOW().init();
+
+// video
+$('#play').on('click', function (e) {
+    e.preventDefault();
+    $("#player")[0].src += "?autoplay=1";
+    $('#player').show();
+    $('#video-cover').hide();
+  })
+  $('#modal1').on('hidden.bs.modal', function (e) {
+    $('#modal1 iframe').attr("src", $("#modal1 iframe").attr("src"));
+  });
